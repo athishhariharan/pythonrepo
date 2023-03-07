d4c91324a72571c08dde181a34d1dd32f82eb6c9
athishhariharan <athishhari2011@gmail.com>
2023-03-07 19:42:31 +1000

    new datatypes file created

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# fruits = ["apple", "banana", "orange"]
+# for fruit in fruits:
+#     print(fruit)
+
+
+# roll_no = input("Enter a number: ")
+# name = input("Enter a name: ")
+# mark = input("Enter mark: ")
+# print("roll_no entered:", roll_no)
+# print("Name entered:", name)
+# print("Mark entered:", mark)
+
+x = input("Enter X val:") 
+y = input("Enter Y val:")
+z = x + y
+print(z)
+
+
