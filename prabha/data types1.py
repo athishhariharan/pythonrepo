6b22ef2765dae7e196744458b5f8318516da0c98
athishhariharan <athishhari2011@gmail.com>
2023-03-11 09:38:16 +1000

    datatypes all workout

---
+++ prabha/data types1.py
@@ -0,0 +1,37 @@
+# integer
+# a = int(input("enter a number"))
+# print(a)
+# print(type(a))
+
+#float
+# a = float(input("enter a number"))
+# print(a)
+# print(type(a))
+
+#type
+# a = "prabha"
+# b = 67
+# c = 78.9
+# d = True
+# print(type(c))
+# print(type(a))
+# print(type(d))
+
+# List
+# colours = ["red","green","blue"]
+# print(colours)
+
+# fruits = ("apple","banana","orange")
+# print(fruits)
+
+
+#multiple variables
+# x,y,z = 5,10.5,"prabha"
+# print(x)
+# print(y)
+# print(z)
+
+# dictionary
+# person = {"name":"prabha", "age":37, "gender":"female", "country":"australia"}
+# print(person)
+
