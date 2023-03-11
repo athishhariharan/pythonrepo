a61b2182df7983fe67b14515ecb2b8cdceb0161d
athishhariharan <athishhari2011@gmail.com>
2023-03-11 10:17:54 +1000

    mixed variables, datatypes workout

---
+++ prabha/combine varibles, datatype,operators.py
@@ -0,0 +1,30 @@
+#add two numbers use variable
+num1 = 10
+num2= 5
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+
+
+#area of rectangle
+# length = 10
+# width = 6
+# area = length*width
+# print("The area of rectangle with", length, "and", width, "is", area)
+
+
+
+#concatenating two strings
+# first_name = "prabha"
+# last_name = "subbaian"
+# full_name = first_name + " " + last_name
+# print("Hi, my name is", full_name )
+
+
+#getting user input for variables
+name =input("what is your name")
+age =int(input("what is your age"))
+height =float(input("what is your height in centimetre"))
+print("your name is", name, "and you are", age, "years old." "your height is", height, "centimetre.")
+      
+
+
