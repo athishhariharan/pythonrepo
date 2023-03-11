2333bb2e26d7dace8fc0d85217f7d37b9663230c
athishhariharan <athishhari2011@gmail.com>
2023-03-11 11:49:38 +1000

    operators workout

---
+++ prabha/operators.py
@@ -0,0 +1,22 @@
+#arithmetic operator
+# a = 10
+# b = 24
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+
+#comparison operator
+# a = 10
+# b = 3
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+
