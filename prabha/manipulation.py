d6886a3132b5ef46524b7592c479efd52b2c2bd9
athishhariharan <athishhari2011@gmail.com>
2023-03-11 14:02:51 +1000

    manipulation string workout

---
+++ prabha/manipulation.py
@@ -0,0 +1,42 @@
+#changing case
+# string = "prabha subbaian"
+# print(string.upper())
+# print(string.lower())
+
+
+#joining 
+# string1 = "athish"
+# string2 = "hariharan"
+# print(" ".join([string1, string2]))
+
+#spliting
+# string1="prabha,athish,tanish"
+# print(string1.split(" "))
+
+#reverse
+# string = "prabha subbaian"
+# print(string[::-1])
+
+# list = [1,2,3,4,5]
+# print(list[::-1])
+
+
+#access character in string
+# word = "prabha"
+# print(word[0])
+# print(word[4])
+
+
+#length of string
+# word = "prabha"
+# print(len(word))
+
+#find character in string
+# word = "prabha"
+# print(word.find("h"))
+
+
+#counting string
+# word = "prabha"
+# print(word.count("a"))
+
