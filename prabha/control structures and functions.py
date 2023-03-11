b8cfac7cf7646fb69f23e883811c53b4710e8a4f
athishhariharan <athishhari2011@gmail.com>
2023-03-12 08:27:42 +1000

    control structures work out

---
+++ prabha/control structures and functions.py
@@ -0,0 +1,22 @@
+# if statement
+# age = 12
+# if age >= 18:
+#      print("you are an adult!")
+# else:
+#      print("you are not an adult yet")
+
+
+# for loops
+# for i in range(5):
+#     print("prabha")
+
+# while loop
+# count = 0
+# while count < 5:
+#     print(count)
+#     count +=5
+
+
+
+
+
