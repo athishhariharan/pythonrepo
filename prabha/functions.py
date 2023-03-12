a513c1f1939a633aec53cc7d69cda301af710af7
athishhariharan <athishhari2011@gmail.com>
2023-03-12 12:21:28 +1000

    fuctions examples def & call

---
+++ prabha/functions.py
@@ -0,0 +1,14 @@
+#def functions
+# def greet(name):
+#     print("hi," + name + "!")
+# greet("prabha")
+
+# def add_numbers(x,y):
+#     sum = x + y
+#     return sum
+#  #calling function
+# result = add_numbers(464,987)
+# print(result)
+
+
+
