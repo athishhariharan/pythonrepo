96a4fe4b92105255b081b548a6b42f26dc2330f5
athishhariharan <athishhari2011@gmail.com>
2023-03-15 13:35:28 +1000

    loop execercise

---
+++ prabha/dictionary_excercise.py
@@ -0,0 +1,24 @@
+import pdb
+
+student_information = {
+    "roll_no": 101,
+    "name": "Athish",
+    "age": 12,
+    "father_name": "Hariharan",
+    "city": "Brisbane"
+    }
+
+for key,value in student_information.items():
+    print(value)
+
+#pdb.set_trace()
+
+#baby_items = ['milk', 'toys','feeding_bottle']
+
+# for item in baby_items:
+#     print(item)
+
+
+
+
+# print(student_data["father_name"])
