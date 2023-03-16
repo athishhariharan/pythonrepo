3d9188ea0dc6ce4e23e43135e29b754c8dae84f0
athishhariharan <athishhari2011@gmail.com>
2023-03-16 14:28:22 +1000

    if and else workout exer

--- prabha/while_execercise.py
+++ prabha/while_execercise.py
@@ -1,7 +1,16 @@
-import pdb
-i = 0
+# import pdb
+# i = 0
 
-while i < 5: # 0 < 5 , 1 < 5, 2 < 5, 3 < 5, 4 < 5, 5 < 5
-    print(i)
-    i += 1 
+# while i < 5: # 0 < 5 , 1 < 5, 2 < 5, 3 < 5, 4 < 5, 5 < 5
+#     print(i)
+#     i += 1 
+
+
+
+# Define a dictionary
+# my_dict = {'apple': 3, 'banana': 2, 'orange': 1}
+
+# # Iterate over the dictionary using a for loop and the items() method
+# for key, value in my_dict.items():
+#     print(key, value)
 
