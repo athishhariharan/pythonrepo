35598a36640587ca95eaa0aaf2598d4ea4d9b26a
athishhariharan <athishhari2011@gmail.com>
2023-04-03 16:29:14 +1000

    renamed

---
+++ prabha/homework_from_dhanraj.py
@@ -0,0 +1,22 @@
+# simple calculator
+
+
+
+
+
+
+
+
+
+
+
+
+num 1 = int(input("Enter the first number:")
+num 2 = int(input("Enter the second number:"))
+operator = input("Enter the operator(+, -, *, /):")
+if num 1 == num2:
+print("The sum of the numberes is:")
+elif num 1 
+
+
+
