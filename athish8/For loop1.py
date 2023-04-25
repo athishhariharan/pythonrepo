c451e29f461899b2ac574bb95b4eb48db3c596ca
athishhariharan <athishhari2011@gmail.com>
2023-04-25 15:58:22 +1000

    even numbers from 1 to 20

---
+++ athish8/For loop1.py
@@ -0,0 +1,8 @@
+# Print even numbers from 1 to 20
+# Use for loop to iterate over a sequence of values
+# Print only even numbers
+
+num = 0
+for num in range (1,21):
+    if num % 2 == 0:
+        print( num)
