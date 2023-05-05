b16ce103a5aa4f4fc865616de4b491ceb7ec78ba
athishhariharan <athishhari2011@gmail.com>
2023-05-05 19:23:49 +1000

    1-20_odd_numbers

---
+++ athish8/1  - 20_odd_numbers.py
@@ -0,0 +1,8 @@
+# Iterate over numbers from 1 to 20
+# Check if the number is even
+# If it's even, skip it to the next iteration 
+# If it's odd,print it
+for i in range(1,21):
+    if i % 2 == 0:
+        continue
+    print(i)
