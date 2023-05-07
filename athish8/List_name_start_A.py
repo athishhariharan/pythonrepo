9f9928248ba41e5759df718e672551538f76c672
athishhariharan <athishhari2011@gmail.com>
2023-05-07 20:18:32 +1000

    Name_start_A

---
+++ athish8/List_name_start_A.py
@@ -0,0 +1,8 @@
+# Define a list of names
+# Loop through the list using a for loop
+# Check if the current names starts with the letter 'A'
+# If the current name starts with the letter 'A', print it
+names = ["Athish", "Tanish", "Prabha", "Hari"]
+for name in names:
+    if name[0] == 'A':
+        print(name)
