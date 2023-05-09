6453cdd90c8fd6790784dd1f453cff490f7c9fcb
athishhariharan <athishhari2011@gmail.com>
2023-05-09 21:34:08 +1000

    Numbers_calculate_sum

---
+++ athish8/List_numbers_input_calculate_sum.py
@@ -0,0 +1,10 @@
+# Define a list of numbers
+# Initialize a variable to hold the sum of the numbers
+# Loop through each number in the list
+# Add the current number to the sum
+# Print the sum of the numbers
+num_list = [1,2,3,4,5]
+sum = 0
+for num in num_list:
+    sum += num
+    print(sum)
