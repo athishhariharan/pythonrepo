03fb304c8f46a225200da3f2e631e21422e2d6f5
athishhariharan <athishhari2011@gmail.com>
2023-05-10 19:22:11 +1000

    Strings_length_greater_5

---
+++ athish8/List_strings_length_greater_or_5.py
@@ -0,0 +1,8 @@
+# Define a list of strings
+# Loop through each string in the list
+# Check if the length of the current string is greater than or equal to 5
+# If it is, print the string
+str_list = ["hello","world","python's","the","best"]
+for string in str_list:
+    if len(string) >= 5:
+        print(string)
