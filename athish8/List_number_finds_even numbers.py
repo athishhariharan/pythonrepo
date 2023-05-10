9a0618d8329711fe2d69ced237ba837ae2fa6047
athishhariharan <athishhari2011@gmail.com>
2023-05-10 19:41:17 +1000

    Find_even_numbers

---
+++ athish8/List_number_finds_even numbers.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+# Loop through each number in the list 
+# Check if the current number is even
+# If it is, print the number
+num_list = [2,5,8,6,9,7,4]
+for num in num_list:
+    if num % 2 == 0:
+        print(num)
