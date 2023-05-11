a60c28f80693dde7509fb54bd686fc055a199fec
athishhariharan <athishhari2011@gmail.com>
2023-05-11 18:31:23 +1000

    Print_reverse_string

---
+++ athish8/List_string_prints_reverse_string.py
@@ -0,0 +1,7 @@
+# Define a list of strings
+# Loop through each string in the list
+# Reverse the current string and print it out
+str_list = ["hello","world","athish's","the","best"]
+for string in str_list:
+    print(string[::-1])
+    
