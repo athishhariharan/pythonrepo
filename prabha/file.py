e8e70209aaac12c0cb5e47f2f66b597e0ace5384
athishhariharan <athishhari2011@gmail.com>
2023-05-12 13:29:29 +1000

    read and write file

--- prabha/file.py
+++ prabha/file.py
@@ -1,14 +1,19 @@
+file = open("C:/athishwork/code/pythonrepo/prabha/example.txt","r")
+content = file.read()
 
-# lists = open("C:/Users/athis/OneDrive/Desktop/store_names.py", "r")
-# print(lists.read())
-# lists.close()
+fw = open("C:/athishwork/code/pythonrepo/prabha/example1.txt","w")
+content1 = fw.write("This is new content written")
 
+file.close()
+fw.close()
+print(content)
+ 
+file1 = open("C:/athishwork/code/pythonrepo/prabha/example1.txt","r")
+content1 = file1.read()
+file1.close()
+print(content1)
 
+file2 = open("C:/athishwork/code/pythonrepo/prabha/example1.txt","a")
+content3 = file2.write("FIle is appended in second time++ Dhanraj Subbaian Athish Tanish Prabhas \n")
 
-# lists = open("C:/Users/athis/OneDrive/Desktop/store_names.py", "r")
-# print(lists.readlines()[3])
-# lists.close()
-
-
-
-
+file2.close()
