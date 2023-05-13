f81dc9fb4cd936fdc5fcd650692bb62ec26e5439
athishhariharan <athishhari2011@gmail.com>
2023-05-13 18:04:17 +1000

    File_read_practice

---
+++ athish8/File_read_practice.py
@@ -0,0 +1,14 @@
+file = open("C:/athishwork/code/pythonrepo/athish8/Average of numbers.py","r")
+content = file.readlines()[8]
+file.close()
+print(content)
+
+file = open("C:/athishwork/code/pythonrepo/athish8/Average of numbers.py","r")
+content = file.readline()
+file.close()
+print(content)
+
+file = open("C:/athishwork/code/pythonrepo/athish8/Average of numbers.py","r")
+content = file.read()
+file.close()
+print(content)
