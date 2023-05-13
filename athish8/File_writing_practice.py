641a237d0949bfa44f3e48e3c8da97c4d9b73bd1
athishhariharan <athishhari2011@gmail.com>
2023-05-13 18:29:25 +1000

    File_writing_practise

---
+++ athish8/File_writing_practice.py
@@ -0,0 +1,11 @@
+file = open("C:/athishwork/code/pythonrepo/athish8/example.txt","w")
+file.write("I am Athish \n")
+file.close()
+
+file = open("C:/athishwork/code/pythonrepo/athish8/example.txt","a")
+file.write("I am Prabha \n")
+file.close()
+
+# file = open("C:/athishwork/code/pythonrepo/athish8/example.txt","a")
+# file.writelines()[5]
+# file.close()
