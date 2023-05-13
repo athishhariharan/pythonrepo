6b30ed0b5da4ac927e345160daee2f51cfd5d6d6
athishhariharan <athishhari2011@gmail.com>
2023-05-13 18:17:41 +1000

    File_write_practise

---
+++ athish8/File_write_practice.py
@@ -0,0 +1,7 @@
+file = open("C:/athishwork/code/pythonrepo/athish8/1  - 20_odd_numbers.py","w")
+file.write("This is Athish \n")
+file.close()
+
+file = open("C:/athishwork/code/pythonrepo/athish8/1  - 20_odd_numbers.py","a")
+file.write("This is Tanish\n")
+file.close()
