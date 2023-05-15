282e6124deda7eb6e9e7487a9b257945a792e339
athishhariharan <athishhari2011@gmail.com>
2023-05-16 07:23:31 +1000

    write_use_append_if

--- prabha/write_use_loop_if.py
+++ prabha/write_use_loop_if.py
@@ -1,11 +1,11 @@
 # Create a list of data
-data = ['Apple', 'Banana', 'Cherry', 'Durian']
+data = ['Apple', 'Banana', 'Cherry', 'Durian', 'Orange', 'fig', 'dates']
 
 # Using with statement
-with open('output.txt', 'w') as file:
-    # Write each item to the file
+with open('output.txt', 'a') as file:
+    # Append each item to the file
     for item in data:
         # Apply some condition to filter the items
         if len(item) > 5:
-            # Write the filtered items
+            # Append the filtered items
             file.write(item + '\n')
