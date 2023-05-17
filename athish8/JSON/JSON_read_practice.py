93bedf0c37100d031e8226e82afba8d79196f9bf
athishhariharan <athishhari2011@gmail.com>
2023-05-17 20:21:23 +1000

    JSON_read_practice

---
+++ athish8/JSON/JSON_read_practice.py
@@ -0,0 +1,4 @@
+import json
+with open("C:/athishwork/code/pythonrepo/athish8/JSON/data.json","r") as file:
+       content = json.load(file)
+print(content)
--- prabha/without_debbugging_example.py
+++ prabha/without_debbugging_example.py
@@ -1,8 +1,9 @@
+# Example code with breakpoints
 def add_numbers(a, b):
-    result = a + b
+    result = a + b  # Set a breakpoint here
     return result
 
 x = 5
 y = 3
-z = add_numbers(x, y)
+z = add_numbers(x, y)  # Set a breakpoint here
 print(z)
