7f1105fb4e258de962b70456c3ecd431efa672d7
athishhariharan <athishhari2011@gmail.com>
2023-05-18 06:29:43 +1000

    debug_practice

---
+++ athish8/JSON/debug_practice.py
@@ -0,0 +1,6 @@
+def calculate_sum(a, b):
+    sum = a + b
+    return sum
+
+result = calculate_sum(5, 10)
+print(result)
--- prabha/without_debbugging_example.py
+++
@@ -1,9 +0,0 @@
-# Example code with breakpoints
-def add_numbers(a, b):
-    result = a + b  # Set a breakpoint here
-    return result
-
-x = 5
-y = 3
-z = add_numbers(x, y)  # Set a breakpoint here
-print(z)
