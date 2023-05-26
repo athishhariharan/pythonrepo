4ea6bba9a85961680a37760c587d51d6e8f05f11
athishhariharan <athishhari2011@gmail.com>
2023-05-26 21:34:27 +1000

    module_practice

---
+++ prabha/calculator_module.py
@@ -0,0 +1,20 @@
+# Simple Calculator Program for Beginners
+
+# Addition
+def add(num1, num2):
+    return num1 + num2
+
+# Subtraction
+def subtract(num1, num2):
+    return num1 - num2
+
+# Multiplication
+def multiply(num1, num2):
+    return num1 * num2
+
+# Division
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero!"
---
+++ prabha/convert_python_module.py
@@ -0,0 +1,15 @@
+import calculator_module
+num1 = 5
+num2 = 3
+
+sum_result = calculator_module.add(num1, num2)
+print("The sum is:", sum_result)
+
+difference_result = calculator_module.subtract(num1, num2)
+print("The difference is:", difference_result)
+
+product_result = calculator_module.multiply(num1, num2)
+print("The product is:", product_result)
+
+division_result = calculator_module.divide(num1, num2)
+print("The division result is:", division_result)
