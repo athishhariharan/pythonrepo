639129ba53d104c226f842e020dea8883c538265
athishhariharan <athishhari2011@gmail.com>
2023-05-27 11:44:36 +1000

    package_practice

---
+++ prabha/main1.py
@@ -0,0 +1,10 @@
+# main.py
+
+import math_operations
+
+# Calculate the square of a number
+number = 5
+result = math_operations.square(number)
+
+# Print the result
+print(f"The square of {number} is: {result}")
